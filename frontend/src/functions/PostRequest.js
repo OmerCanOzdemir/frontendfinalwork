ae8cb32508b6c13b26ae968a55133b1d123d2756
Omer Can Özdemir <omer.can.ozdemir@student.ehb.be>
2022-06-14 10:43:55 +0200

    feat: add headers

--- frontend/src/functions/PostRequest.js
+++ frontend/src/functions/PostRequest.js
@@ -4,7 +4,7 @@ import axios from "axios";
 async function PostRequest(url, body) {
 
         try {
-                const response = await axios.post(url, body,{ headers: { "authorisation": 00000000-0000-0000-0000-000000000000 } });
+                const response = await axios.post(url, body,{ headers: { "authorisation": "00000000-0000-0000-0000-000000000000" } });
                 return response.data;
         } catch (error) {
                 console.log(error);
--- frontend/src/functions/PutRequest.js
+++ frontend/src/functions/PutRequest.js
@@ -4,7 +4,7 @@ import axios from "axios";
 
 async function PutRequest(url, body) {
     try {
-        var response = await axios.put(url, body,{ headers: { "authorisation": 00000000-0000-0000-0000-000000000000 } });
+        var response = await axios.put(url, body,{ headers: { "authorisation": "00000000-0000-0000-0000-000000000000" } });
         return response.data;
     } catch (error) {
         console.log(error);
